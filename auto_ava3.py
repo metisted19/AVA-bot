3f316c133f8695a6dcd7e7e06ed5223e6df79cc4
metisted19 <ton_email_github@example.com>
2025-04-14 11:47:09 +0200

    Ajout des nouveaux actifs boursiers à AVA

--- auto_ava3.py
+++ auto_ava3.py
@@ -10,7 +10,8 @@ from sklearn.metrics import accuracy_score
 import xgboost as xgb
 
 # --- TICKERS À TRAITER ---
-tickers = ["aapl", "tsla", "googl", "btc-usd", "eth-usd", "^fchi", "msft", "amzn", "nvda", "^gspc"]
+tickers = ["aapl", "tsla", "googl", "btc-usd", "eth-usd", "^fchi", "msft", "amzn", "nvda", "^gspc","doge-usd", "ada-usd"
+]
 
 # --- CHEMINS DE SAUVEGARDE ---
 os.makedirs("data", exist_ok=True)
