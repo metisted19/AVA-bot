0acf0885a60ae9df7eba0c3ce76ef51da98b71bd
metisted19 <ton_email_github@example.com>
2025-04-17 11:35:04 +0200

    météo complète

--- pages/3_Chat_AVA.py
+++ pages/3_Chat_AVA.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title="Chat AVA", layout="centered")
 import os
 import pandas as pd
 from analyse_technique import ajouter_indicateurs_techniques, analyser_signaux_techniques
