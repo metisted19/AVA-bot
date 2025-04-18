ce7f5e2af4b795b06820e9e75cd6e78874092bb4
metisted19 <ton_email_github@example.com>
2025-04-18 05:15:09 +0200

    semantique

--- pages/3_Chat_AVA.py
+++ pages/3_Chat_AVA.py
@@ -182,25 +182,26 @@ if question:
 
         
         if question:
-            # Ajouter la question dans les messages de session
+            # Ajouter la question de l'utilisateur dans les messages de session
             st.session_state.messages.append({"role": "user", "content": question})
 
-            # Afficher la question de l'utilisateur dans le chat
+            # Affichage de la question dans le chat (utilisateur)
             with st.chat_message("user"):
                 st.markdown(question_raw)
 
             # Traitement de la question en minuscule et nettoyage
             question_clean = question.lower().strip()
 
-            # Appel à la fonction de réponse en utilisant la question nettoyée
+            # Appel à la fonction pour obtenir la réponse de l'IA
             message_bot = obtenir_reponse_ava(question_clean)
 
-            # Afficher la réponse de l'IA dans le chat
+            # Affichage de la réponse de l'IA dans le chat (assistant)
             with st.chat_message("assistant", avatar="assets/ava_logo.png"):
-                st.markdown(message_bot)
+                 st.markdown(message_bot)
 
             # Ajouter la réponse de l'IA dans les messages de session
             st.session_state.messages.append({"role": "assistant", "content": message_bot})
+
  
 
 
